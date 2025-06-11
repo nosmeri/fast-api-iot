aae5b77f049e6da7196e771615863536e2315a32
nosmeri <parkdohyeon622@gmail.com>
2025-06-11 20:38:22 +0900

    update database URL to use environment variable for flexibility

--- config/db.py
+++ config/db.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///app.db"
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL", "sqlite:///./app.db")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
