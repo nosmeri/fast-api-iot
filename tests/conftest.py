9417ba3f9d81bb2e4bd16ece6ef2e88e07614423
nosmeri <parkdohyeon622@gmail.com>
2025-06-20 18:38:31 +0900

    fix: update CI workflow to install dependencies from requirements.txt and remove Alembic migration step

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,19 @@
 import pytest
+from config.settings import settings
 from fastapi.testclient import TestClient
 from main import app
+from models import Base
+from sqlalchemy import create_engine
 
 client = TestClient(app)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_database():
+    engine = create_engine(settings.SQLALCHEMY_DATABASE_URL)
+    Base.metadata.create_all(bind=engine)  # 테이블 생성
+    yield
+    Base.metadata.drop_all(bind=engine)
 
 
 @pytest.fixture(scope="session", autouse=True)
